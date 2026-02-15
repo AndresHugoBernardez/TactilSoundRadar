af6466542a72adec337f658191efc21141f2e305
Andrés Hugo Bernárdez <94204912+AndresHugoBernardez@users.noreply.github.com>
2026-02-15 19:57:25 -0300

    Windows Net form

---
+++ Casco para sordo/SoundRadar/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿using System.Windows.Forms;
+
+namespace SoundRadar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.conexiontoolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.puertoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.noPortToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newPortToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.conexiontoolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // conexiontoolStrip1
+            // 
+            this.conexiontoolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.conexiontoolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.conexiontoolStrip1.Name = "conexiontoolStrip1";
+            this.conexiontoolStrip1.Size = new System.Drawing.Size(894, 25);
+            this.conexiontoolStrip1.TabIndex = 0;
+            this.conexiontoolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.puertoToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(71, 22);
+            this.toolStripDropDownButton1.Tag = "";
+            this.toolStripDropDownButton1.Text = "Conexión";
+            this.toolStripDropDownButton1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.toolStripDropDownButton1.Click += new System.EventHandler(this.toolStripDropDownButton1_Click);
+            // 
+            // puertoToolStripMenuItem
+            // 
+            this.puertoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.noPortToolStripMenuItem,
+            this.newPortToolStripMenuItem});
+            this.puertoToolStripMenuItem.Name = "puertoToolStripMenuItem";
+            this.puertoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.puertoToolStripMenuItem.Text = "Puerto";
+            this.puertoToolStripMenuItem.Click += new System.EventHandler(this.puertoToolStripMenuItem_Click);
+            // 
+            // noPortToolStripMenuItem
+            // 
+            this.noPortToolStripMenuItem.Enabled = false;
+            this.noPortToolStripMenuItem.Name = "noPortToolStripMenuItem";
+            this.noPortToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.noPortToolStripMenuItem.Text = "Sin puertos";
+            this.noPortToolStripMenuItem.Visible = false;
+            this.noPortToolStripMenuItem.Click += new System.EventHandler(this.pToolStripMenuItem_Click);
+            // 
+            // newPortToolStripMenuItem
+            // 
+            this.newPortToolStripMenuItem.Checked = true;
+            this.newPortToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.newPortToolStripMenuItem.Name = "newPortToolStripMenuItem";
+            this.newPortToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.newPortToolStripMenuItem.Text = "asdf";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 16;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 25);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 16;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(894, 376);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 401);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.conexiontoolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.conexiontoolStrip1.ResumeLayout(false);
+            this.conexiontoolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip conexiontoolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem puertoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem noPortToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ToolStripMenuItem newPortToolStripMenuItem;
+    }
+}
+
---
+++ Casco para sordo/SoundRadar/Form1.cs
@@ -0,0 +1,347 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoundRadar
+{
+    public partial class Form1 : Form
+    {
+
+        private SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += (sender, e) =>
+            {
+                PopulateTableWithPanels();
+                InitializeSerialAndLog(); // inicializa el log y el puerto serie
+            };
+        }
+
+        private void InitializeSerialAndLog()
+        {
+            // Elimina y dispone los items dinámicos (mantén noPortToolStripMenuItem)
+            for (int i = puertoToolStripMenuItem.DropDownItems.Count - 1; i >= 0; i--)
+            {
+                var item = puertoToolStripMenuItem.DropDownItems[i];
+                if (item == noPortToolStripMenuItem)
+                    continue;
+                puertoToolStripMenuItem.DropDownItems.RemoveAt(i);
+                item.Dispose();
+            }
+
+            var ports = SerialPort.GetPortNames();
+
+            if (ports.Length == 0)
+            {
+                this.noPortToolStripMenuItem.Visible = true;
+                return;
+            }
+
+            this.noPortToolStripMenuItem.Visible = false;
+
+            // Añade un ToolStripMenuItem por cada puerto disponible
+            foreach (var p in ports)
+            {
+                var portName = p; // copia local para evitar captura en el lambda
+                var mi = new ToolStripMenuItem(portName)
+                {
+                    Tag = portName,
+                    Checked = false
+                };
+                mi.Click += Checking;
+                puertoToolStripMenuItem.DropDownItems.Add(mi);
+            }
+        }
+
+        private void Checking(object sender, EventArgs e)
+        {
+            if (!(sender is ToolStripMenuItem clicked))
+                return;
+
+            // Si estaba marcado -> desmarcar todo y cerrar puerto
+            if (clicked.Checked)
+            {
+                foreach (ToolStripItem item in puertoToolStripMenuItem.DropDownItems)
+                {
+                    if (item is ToolStripMenuItem mi) mi.Checked = false;
+                }
+                CloseSerial();
+                return;
+            }
+
+            // Marcar solo el pulsado y abrir
+            foreach (ToolStripItem item in puertoToolStripMenuItem.DropDownItems)
+            {
+                if (item is ToolStripMenuItem mi) mi.Checked = false;
+            }
+
+            clicked.Checked = true;
+            var portName = clicked.Tag as string ?? clicked.Text;
+            StartSerial(portName, 9600);
+        }
+
+        private void StartSerial(string portName, int baudRate = 9600)
+        {
+            if (string.IsNullOrWhiteSpace(portName))
+                return;
+
+            try
+            {
+                if (serialPort != null)
+                {
+                    CloseSerial();
+                }
+
+                serialPort = new SerialPort(portName, baudRate)
+                {
+                    Encoding = Encoding.ASCII,
+                    NewLine = "\n",
+                    ReadTimeout = 500
+                };
+
+                serialPort.DataReceived += SerialPort_DataReceived;
+                serialPort.Open();
+            }
+            catch (Exception)
+            {
+                if (serialPort != null)
+                {
+                    try { serialPort.Dispose(); } catch { }
+                    serialPort = null;
+                }
+            }
+        }
+
+        private void CloseSerial()
+        {
+            try
+            {
+                if (serialPort != null)
+                {
+                    try
+                    {
+                        serialPort.DataReceived -= SerialPort_DataReceived;
+                        if (serialPort.IsOpen)
+                            serialPort.Close();
+                    }
+                    catch { }
+                    try
+                    {
+                        serialPort.Dispose();
+                    }
+                    catch { }
+                    serialPort = null;
+                }
+            }
+            catch (Exception)
+            {
+                // opcional: log
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string line;
+                try
+                {
+                    line = serialPort.ReadLine();
+                }
+                catch (TimeoutException)
+                {
+                    line = serialPort.ReadExisting();
+                }
+
+                if (!string.IsNullOrEmpty(line))
+                {
+                    // Marshal seguro a UI
+                    if (this.IsHandleCreated && !this.IsDisposed)
+                    {
+                        this.BeginInvoke(new Action<string>(ProcesarComandoSerie), line);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // opcional: log
+            }
+        }
+
+        // Posiciones
+        private int positionL = 0;
+        private int positionR = 0;
+        private int positionF = 0;
+        private int positionB = 0;
+
+        private void ProcesarComandoSerie(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input) || tableLayoutPanel1 == null)
+                return;
+
+            // Procesa cada línea completa que venga (evita loops infinitos)
+            var lines = input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                // Mantén la actualización de UI agrupada para evitar parpadeo
+                tableLayoutPanel1.SuspendLayout();
+                this.SuspendLayout();
+
+                // Pinta rastro anterior (lo mantenías en azul)
+                SafeColorearAzul(positionL, positionF);
+                SafeColorearAzul(positionR, positionF);
+                SafeColorearAzul(positionL, positionB);
+                SafeColorearAzul(positionR, positionB);
+
+                // Parse seguro: cada comando es una letra L/R/F/B seguida de un dígito opcional 0..7
+                for (int i = 0; i < line.Length; i++)
+                {
+                    char letra = line[i];
+                    if ((letra == 'L' || letra == 'R' || letra == 'F' || letra == 'B') && i + 1 < line.Length)
+                    {
+                        char d = line[++i];
+                        int val = -1;
+                        if (d >= '0' && d <= '7')
+                            val = d - '0';
+
+                        switch (letra)
+                        {
+                            case 'L':
+                                positionL = (val >= 0) ? 7 - val : 7;
+                                break;
+                            case 'R':
+                                positionR = (val >= 0) ? 8 + val : 8;
+                                break;
+                            case 'F':
+                                positionF = (val >= 0) ? 7 - val : 7;
+                                break;
+                            case 'B':
+                                positionB = (val >= 0) ? 8 + val : 8;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        // Ignorar caracteres no reconocidos
+                    }
+                }
+
+                // Colorea nueva posición en rojo
+                SafeColorearRojo(positionL, positionF);
+                SafeColorearRojo(positionR, positionF);
+                SafeColorearRojo(positionL, positionB);
+                SafeColorearRojo(positionR, positionB);
+
+                tableLayoutPanel1.ResumeLayout(false);
+                this.ResumeLayout(false);
+                this.PerformLayout();
+            }
+        }
+
+        // Métodos seguros para colorear (comprueban índices y nulls)
+        private bool IsValidCell(int col, int row)
+        {
+            return tableLayoutPanel1 != null
+                && col >= 0 && col < tableLayoutPanel1.ColumnCount
+                && row >= 0 && row < tableLayoutPanel1.RowCount;
+        }
+
+        private void SafeColorearAzul(int a, int b)
+        {
+            if (!IsValidCell(a, b)) return;
+            var ctl = tableLayoutPanel1.GetControlFromPosition(a, b);
+            if (ctl != null) ctl.BackColor = Color.DarkSlateBlue;
+        }
+
+        private void SafeColorearRojo(int a, int b)
+        {
+            if (!IsValidCell(a, b)) return;
+            var ctl = tableLayoutPanel1.GetControlFromPosition(a, b);
+            if (ctl != null) ctl.BackColor = Color.DarkRed;
+        }
+
+        private void SafeDecolorar(int a, int b)
+        {
+            if (!IsValidCell(a, b)) return;
+            var ctl = tableLayoutPanel1.GetControlFromPosition(a, b);
+            if (ctl != null) ctl.BackColor = SystemColors.Control;
+        }
+
+        private void PopulateTableWithPanels()
+        {
+            if (tableLayoutPanel1 == null)
+                return;
+
+            tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+
+            tableLayoutPanel1.Controls.Clear();
+
+            int cols = tableLayoutPanel1.ColumnCount;
+            int rows = tableLayoutPanel1.RowCount;
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    var panel = new Panel
+                    {
+                        Dock = DockStyle.Fill,
+                        Margin = new Padding(1),
+                        BackColor = SystemColors.ControlDark,
+                        Tag = new Point(c, r)
+                    };
+
+                    panel.Click += Panel_Click;
+
+                    tableLayoutPanel1.Controls.Add(panel, c, r);
+                }
+            }
+
+            tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        private void Panel_Click(object sender, EventArgs e)
+        {
+            // Puedes reactivar el toggling si quieres
+        }
+
+        private void toolStripDropDownButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void puertoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
